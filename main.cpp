a573be978f9fb6b874af3612dfa03eb5abd7add2
Rus <ruslan_job@list.ru>
2023-05-02 06:18:23 +0300

    copy_of_example

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+using namespace std;
+
+#include <iostream>
+
+int main (int, char **) {
+    cout << "Version: " << version() << endl;
+    cout << "Hello, world!" << endl;
+    cout << "Modification" << endl;
+    return 0;
+}
